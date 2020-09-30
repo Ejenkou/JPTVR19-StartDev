ebfb270d58dfc0a171fc7cd13fda20e165e91c2b
Your Name <you@example.com>
2020-09-30 23:18:21 +0300

    First commit

--- src/startdev/Task3.java
+++ src/startdev/Task3.java
@@ -16,33 +16,35 @@ public class Task3 {
         System.out.println("----- Задача 3 ------");
         System.out.println("Создаем одномерный массив  из 10 ячеек типа int, \nзаполняем его случайными числами и выводим \nего значения в цикле");
         Random random = new Random();
-        int myArr[] = new int [10] ;
+        int myArr[] = new int[10];
         int sum = 0;
         int maxNumber = 50;
         int minNumber = 0;
-                
-        for(int = 0; i < myArr.lenght; i++){
+        for (int i = 0; i < myArr.length; i++) {
             myArr[i] = random.nextInt(49 + 1);
         }
-        for(int = 0; i < myArr.lenght; i++){
-            System.out.println("%4d", myArr[i]);
+
+        for (int i = 0; i < myArr.length; i++) {
+            System.out.printf("%4d", myArr[i]);
+        }
+        for (int i = 0; i < myArr.length; i++) {
+            sum = sum + myArr[i];
         }
         System.out.println(sum);
-        
-          for(int = 0; i < myArr.lenght; i++){
-            if(maxNumber < myArr[i]) {
-               maxNubmer = myArr[i];
-               
-            } if (minNumer < myArr[i]) {
+
+        for (int i = 0; i < myArr.length; i++) {
+            if (maxNumber < myArr[i]) {
+                maxNumber = myArr[i];
+            } if (minNumber < myArr[i]) {
                 minNumber = myArr[i];
             }
           
             
-    }
+        }
         
         System.out.println("Сумма всех чисел массива: ");
         System.out.println("min = " + minNumber);
-        System.out.println("max = "+ maxNumber);
+        System.out.println("max = " + maxNumber);
         System.out.println("----- конец задачи 3 ------");
     }
 }
